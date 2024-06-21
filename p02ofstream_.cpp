7214113961a786d1f160d3093cf710c7c6a8d17e
oscarfg83 <43353602+oscarfg83@users.noreply.github.com>
2024-06-21 16:54:10 -0500

    Add files via upload

---
+++ p02ofstream_.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+	std::ofstream archivo("datos.txt"); // Abre el archivo "datos.txt" para escritura
+	
+	if (archivo.is_open()) {
+		// Realizar operaciones de escritura en el archivo
+		archivo << "AL  2da!" << std::endl;
+		
+		archivo.close(); // Cierra el archivo después de su uso
+	} else {
+		std::cout << "No se pudo abrir el archivo." << std::endl;
+	}
+	
+	return 0;
+}
